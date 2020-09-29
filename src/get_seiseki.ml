0509970e2c348155ad2312bfeaa6b788b9d47cac
nsbt <55523468+nsbt@users.noreply.github.com>
2020-09-29 15:53:15 +0900

    create get_seiseki.ml

---
+++ src/get_seiseki.ml
@@ -0,0 +1,19 @@
+(* 学生ひとり分のデータを表す型 *)
+type gakusei_t = {
+    namae : string; (* 名前 *)
+    tensuu : int; (* 点数 *)
+    seiseki : string; (* 成績 *)
+}
+
+(* 学生データの例 *)
+let gakusei1 = {namae="asai"; tensuu=70; seiseki="B"}
+let gakusei2 = {namae="tanaka"; tensuu=90; seiseki="A"}
+let gakusei3 = {namae="yamada"; tensuu=60; seiseki="C"}
+
+(* 目的：指定された学生の成績を返す *)
+(* get_seiseki : string -> gakusei_t list -> string option *)
+let rec get_seiseki name lst = match lst with
+    [] -> None
+    | {namae=n; tensuu=t; seiseki=s} :: rest ->
+        if name = n then Some (s)
+        else get_seiseki rest
